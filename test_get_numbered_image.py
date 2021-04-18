fbc34efd2c9b8eaa8d81b4db39828f3063a28c9e
Mauritian <39094442+Eldiias@users.noreply.github.com>
2021-04-18 21:13:44 +0200

    updated numberedpic

--- test_get_numbered_image.py
+++ test_get_numbered_image.py
@@ -6,7 +6,9 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from sklearn.cluster import MiniBatchKMeans
 from mpl_toolkits.axes_grid1 import ImageGrid
-
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
 
 def get_image_from_web(x:str):
     image=r.get(x).content
@@ -34,34 +36,40 @@ def get_new_image(arr:np.array, n:int)->np.array:
     labels=mkm.labels_
     values=mkm.cluster_centers_
     img_compressed=values[labels].reshape(arr.shape).round()
-
+    
+    new_image=np.array(Image.fromarray(np.uint8(img_compressed)).convert('RGB').filter(ImageFilter.CONTOUR))
     Image.fromarray(np.uint8(img_compressed)).convert('RGB').filter(ImageFilter.CONTOUR).show()
 
-    border=(img_compressed[1:,1:,0]==img_compressed[:-1,1:,0]) & (img_compressed[1:,1:,0]==img_compressed[1:,:-1,0])
-    border=np.vstack((
-        np.hstack((
-            [False], 
-            img_compressed[0,1:,0]==img_compressed[0,:-1,0])).reshape((1,-1)),
-        np.hstack((
-            (img_compressed[1:,0,0]==img_compressed[:-1,0,0]).reshape((-1,1)),
-             border))))
+    vertical=np.vstack((np.ones(new_image.shape[1]),new_image[1:,:,0]!=new_image[:-1,:,0]))
+    horizontal=np.hstack((np.ones((new_image.shape[0],1)),new_image[:,1:,0]!=new_image[:,:-1,0]))
+    numbers_to_be_displayed=vertical*horizontal
+    zzz=pd.DataFrame((labels+1).reshape(new_image.shape[:2])*numbers_to_be_displayed)
+    zzzzz=zzz.reset_index().melt('index').copy()
+    zzzzz=zzzzz.query('value>0').copy()
+    zzzzz=zzzzz.rename(columns={'index':'row', 'variable':'column'})
+    zzzzz['value']=zzzzz['value'].astype(int).astype(str)
+    zzzzz.row=zzz.shape[0]-zzzzz.row
 
+    scatter=px.scatter(zzzzz, x='column', y='row', text='value', opacity=0)
+    scatter=scatter.update_traces(textfont_size=10)
 
+    fig = go.Figure()
+    layout= go.Layout(images= [dict(
+                    source= Image.fromarray(new_image),
+                    xref= "x",
+                    yref= "y",
+                    y=768,
+                    x=0,
+                    sizex= 1024,
+                    sizey= 768,
+                    sizing= "stretch",
+                    opacity= 1,
+                    layer= "below")])
+    fig=go.Figure(data=[scatter.data[0]],layout=layout)
+    fig.show()
 
-    fig = plt.figure(1,(40,80))
-    grid = ImageGrid(fig, 111,
-                    nrows_ncols=(1,3),
-                    axes_pad=0.1,
-                    )
-    loi=[arr.astype(np.uint8), img_compressed.astype(np.uint8), border]
-    for i in range(3):
-        if i==2:
-            grid[i].imshow(loi[i],interpolation='none', cmap='gray')
-        else:
-            grid[i].imshow(loi[i],interpolation='none')
-    plt.show()
     
-    return img_compressed, labels, values, border
+    return img_compressed, labels, values, zzzzz
 
 if __name__=='__main__':
     url=input('Pass the full path to the image or the url please \t')
