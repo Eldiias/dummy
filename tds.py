14b20e85b3443d9e519d605cfa0b6a713f654cd2
Mauritian <39094442+Eldiias@users.noreply.github.com>
2021-04-24 22:54:05 +0200

    requires connecting the graph with the button

--- tds.py
+++ tds.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from helper import *
 import plotly.graph_objects as go
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 import dash
 import dash_core_components as dcc
@@ -11,18 +11,31 @@ from collections import deque
 channel_name = 'kyle'
 app = dash.Dash(__name__)
 
-data = get_data(channel_name, get_counts = True, get_general_info=True, get_preview_image=True, get_stream_info=True)
-print(data)
-
 X = deque(maxlen=50)
-X.append(data.index[-1])
 Y = deque(maxlen=50)
-Y.append(data.current_viewers.values[-1])
+data =''
 
-app.layout = html.Div([
-    html.Div([html.H1(f'Current Number of Viewers for the Channel {data.streamer_id.values[0]}'),
-              html.H2(f'{data.lastBroadcast_title.values[0]}, started on {data.current_created_at.values[0]}')]),
-    html.Div([html.Img(src=data['PreviewImage'][0], width=300)]),
+headers = ''
+def build_plot(channel_name, headers):
+    global X,Y, data
+    data = get_data(channel_name, get_counts = True, get_general_info=True, get_preview_image=True, get_stream_info=True, headers=headers)
+    print(data)
+    X.append(data.index[-1])
+    Y.append(data.current_viewers.values[-1])
+    return None
+
+app.layout = html.Div([html.Div([
+    html.Div(children='''\nName of the channel to track:\t'''),
+    dcc.Input(id='channel', value='', type='text'),
+    html.Div(children='''\nPaste the headers\n'''),
+    dcc.Textarea(id='headers', value=''),
+    html.Button('Submit', id='submit-val', n_clicks=0),
+    ]),
+
+
+    html.Div([html.H1(id='container-button-basic',children = f'waiting for input'),
+              html.H2(id='add_info',children = '')]),
+    html.Div([html.Img(id='image',src='', width=300)]),
     html.Div(children=html.Div(id='graphs'), className='row'),
     dcc.Interval(
         id='graph-update',
@@ -31,13 +44,48 @@ app.layout = html.Div([
 
 
 @app.callback(
+    Output('container-button-basic', 'children'),
+    [Input('submit-val', 'n_clicks')],
+    [State('channel', 'value')])
+def update_output(n_clicks, value):
+    return f'Current Number of Viewers for the Channel {value}'
+
+@app.callback(
+    Output('add_info', 'children'),
+    [Input('submit-val', 'n_clicks')],
+    [State('channel', 'value'),
+     State('headers', 'value')])
+def update_output(n_clicks, value, head):
+    global channel_name, headers, data 
+    headers = head
+    channel_name = value
+    build_plot(channel_name, headers)
+    return f'{data.lastBroadcast_title.values[0]}, started on {data.current_created_at.values[0]}'
+@app.callback(
+    Output('image', 'src'),
+    [Input('submit-val', 'n_clicks')],
+    [State('channel', 'value')])
+def update_output(n_clicks, value):
+    global data 
+    return data['PreviewImage'][0]
+
+
+# @app.callback(
+#     Output('graphs', 'children'),
+#     [Input('submit-val', 'n_clicks')],
+#     [State('channel', 'value')])
+# def update_output(n_clicks, value):
+#     global data 
+#     return data['PreviewImage'][0]
+
+@app.callback(
     dash.dependencies.Output('graphs','children'),
     [dash.dependencies.Input('graph-update', 'n_intervals')]
     )
 def update_graph(n):
     graphs = []
 
-    upd = get_data(channel_name, get_counts = True)
+    upd = get_data(channel_name, headers=headers, get_counts = True)
     X.append(upd.index[-1])
     Y.append(upd.current_viewers.values[-1])
     data = go.Scatter(
@@ -55,4 +103,4 @@ def update_graph(n):
 
     return graphs
 
-app.run_server(debug=True)
+app.run_server(debug=True) 
